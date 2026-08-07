db5fb11b166b6728b7f4a258134c3f58a9dc1854
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-08-07 18:52:35 +0600

    Update init.sh

--- runtime/init.sh
+++ runtime/init.sh
@@ -8,3 +8,17 @@ sudo -v || {
     printf "\n\e[31mAuthentication failed\e[0m\n" >&2
     exit 1
 }
+
+SCRIPT_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+DEPS=(
+  build-essential
+  curl
+  tar
+  unzip
+  ca-certificates
+  python3
+  python3-pip
+  python3-serial
+  libusb-1.0-0
+)
