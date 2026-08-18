5cb083e5d35f380a5e8955ca1def01b6ca14bd29
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-08-18 17:11:20 +0600

    Add files via upload

---
+++ runtime/gen_missing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+    exit 0
+fi
+
+SCRIPT_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+LIB_STORE="$SCRIPT_ROOT/lib_store"
+TMP_LIB_STORE="$SCRIPT_ROOT/lib_store/tmp"
