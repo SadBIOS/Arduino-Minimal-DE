b03f8cb07e6229d6af6376659f4f4a215fa6606d
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-09-01 16:19:29 +0600

    Update firmware_builder.sh

--- runtime/firmware_builder.sh
+++ runtime/firmware_builder.sh
@@ -14,7 +14,7 @@ SRC_CODE=""
 LINUX_BUILD_CONF=""
 ROOT_PATH=""
 PARSED_FQBN=""
-FQBN_OPTIONS=""
+ADDITIONAL_OPTIONS=""
 FINAL_FQBN=""
 CONFIG_LINE=""
 CONFIG_KEY=""
@@ -31,11 +31,15 @@ MATCH_COUNT=0
 MATCH_INDEX=""
 SELECTED_FQBN=""
 CHOICE=""
+BOARD_DETAILS=""
+UNKNOWN_OPTS=""
+IFS_BAK=""
+OPT=""
 
 function load_board_list() {
     BOARD_LIST="$("$BINPATH" --config-file "$CONFIG_FILE" board listall 2>/dev/null)"
     if [[ $? -ne 0 || -z "$BOARD_LIST" ]]; then
-        printf "[\e[31mFAIL\e[0m] Unable to retrieve boards from '%s --config-file %s board listall'.\n" "$BINPATH" "$CONFIG_FILE"
+        printf "[\e[31mFAIL\e[0m] Unable to retrieve boards.\n"
         exit 1
     fi
 }
@@ -82,30 +86,21 @@ function fqbn_exists() {
             return 0
         fi
     done < <(printf '%s\n' "$BOARD_LIST" | awk 'NR > 1 && NF >= 2 { print $NF "\t" substr($0, 1, length($0) - length($NF) - 1) }')
-
     return 1
 }
 
 function update_config_fqbn() {
-    awk -v new_fqbn="$SELECTED_FQBN" '
-        /^[[:space:]]*FQBN[[:space:]]*==/ {
-            sub(/==.*/, "== " new_fqbn)
-        }
-        { print }
-    ' "$LINUX_BUILD_CONF" > "${LINUX_BUILD_CONF}.tmp"
-
+    awk -v new_fqbn="$SELECTED_FQBN" '/^[[:space:]]*FQBN[[:space:]]*==/ { sub(/==.*/, "== " new_fqbn) } { print }' "$LINUX_BUILD_CONF" > "${LINUX_BUILD_CONF}.tmp"
     if [[ $? -ne 0 ]]; then
-        rm -f "${LINUX_BUILD_CONF}.tmp"
+        rm -vf "${LINUX_BUILD_CONF}.tmp"
         printf "[\e[31mFAIL\e[0m] Unable to update FQBN in %s.\n" "$LINUX_BUILD_CONF"
         exit 1
     fi
-
     mv -v "${LINUX_BUILD_CONF}.tmp" "$LINUX_BUILD_CONF"
     if [[ $? -ne 0 ]]; then
         printf "[\e[31mFAIL\e[0m] Unable to replace %s with the updated configuration.\n" "$LINUX_BUILD_CONF"
         exit 1
     fi
-
     PARSED_FQBN="$SELECTED_FQBN"
     printf "[\e[32m OK \e[0m] Updated FQBN in %s to %s\n" "$LINUX_BUILD_CONF" "$SELECTED_FQBN"
 }
@@ -122,12 +117,10 @@ function select_closest_match() {
         printf "     FQBN: %s\n" "$LEV_FQBN"
         printf "     Distance: %s\n\n" "$LEV_DIST"
     done < <(show_closest_matches "$QUERY_FQBN")
-
     if [[ $MATCH_COUNT -eq 0 ]]; then
         printf "[\e[31mFAIL\e[0m] No valid FQBNs were returned by 'board listall'.\n"
         exit 1
     fi
-
     while true; do
         printf "Select a board [1-%d], or 0 to abort: " "$MATCH_COUNT"
         read -r CHOICE
@@ -135,115 +128,34 @@ function select_closest_match() {
             printf "[\e[31mFAIL\e[0m] FQBN selection cancelled.\n"
             exit 1
         fi
-
         if [[ "$CHOICE" =~ ^[0-9]+$ ]] && (( CHOICE >= 1 && CHOICE <= MATCH_COUNT )); then
             MATCH_INDEX=0
             SELECTED_FQBN=""
             while IFS=$'\t' read -r LEV_DIST LEV_FQBN LEV_FULL; do
                 [[ -z "$LEV_FQBN" ]] && continue
                 MATCH_INDEX=$((MATCH_INDEX + 1))
-
                 if [[ "$MATCH_INDEX" -eq "$CHOICE" ]]; then
                     SELECTED_FQBN="$LEV_FQBN"
                     break
                 fi
             done < <(show_closest_matches "$QUERY_FQBN")
-
             if [[ -n "$SELECTED_FQBN" ]]; then
                 update_config_fqbn
                 return 0
             fi
         fi
-
         printf "[\e[31mFAIL\e[0m] Invalid selection. Please choose a number from 1 to %d, or 0 to abort.\n" "$MATCH_COUNT"
     done
 }
 
-function map_config_key() {
-    case "$1" in
-        FQBN)
-            printf '%s' "FQBN"
-        ;;
-
-        CPU_FREQ)
-            printf '%s' "CPUFreq"
-        ;;
-
-        CDC_OPTN)
-            printf '%s' "CDCOnBoot"
-        ;;
-
-        USB_MODE)
-            printf '%s' "USBMode"
-        ;;
-
-        UPLOAD_MODE)
-            printf '%s' "UploadMode"
-        ;;
-
-        FLASH_MODE)
-            printf '%s' "FlashMode"
-        ;;
-
-        FLASH_FREQ)
-            printf '%s' "FlashFreq"
-        ;;
-
-        FLASH_SIZE)
-            printf '%s' "FlashSize"
-        ;;
-
-        PSRAM)
-            printf '%s' "PSRAM"
-        ;;
-
-        PARTITION_SCHEME)
-            printf '%s' "PartitionScheme"
-        ;;
-
-        ERASE_FLASH)
-            printf '%s' "EraseFlash"
-        ;;
-
-        MSC_ON_BOOT)
-            printf '%s' "MSCOnBoot"
-        ;;
-
-        DFU_ON_BOOT)
-            printf '%s' "DFUOnBoot"
-        ;;
-
-        UPLOAD_SPEED)
-            printf '%s' "UploadSpeed"
-        ;;
-
-        DEBUG_LEVEL)
-            printf '%s' "DebugLevel"
-        ;;
-
-        JTAG_ADAPTER)
-            printf '%s' "JTAGAdapter"
-        ;;
-
-        LOOP_CORE)
-            printf '%s' "LoopCore"
-        ;;
-
-        EVENTS_CORE)
-            printf '%s' "EventsCore"
-        ;;
-    esac
-}
-
 function parse_config() {
     PARSED_FQBN=""
-    FQBN_OPTIONS=""
+    ADDITIONAL_OPTIONS=""
     while IFS= read -r CONFIG_LINE || [[ -n "$CONFIG_LINE" ]]; do
         CONFIG_LINE="${CONFIG_LINE%$'\r'}"
         if [[ -z "$CONFIG_LINE" || "$CONFIG_LINE" =~ ^[[:space:]]*# ]]; then
             continue
         fi
-
         CONFIG_LINE="${CONFIG_LINE%%#*}"
         if [[ "$CONFIG_LINE" =~ ^([^=]+)[[:space:]]*==[[:space:]]*(.*)$ ]]; then
             CONFIG_KEY="${BASH_REMATCH[1]}"
@@ -252,44 +164,50 @@ function parse_config() {
             CONFIG_VAL="$(printf '%s' "$CONFIG_VAL" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
             if [[ "$CONFIG_KEY" == "FQBN" ]]; then
                 PARSED_FQBN="$CONFIG_VAL"
-            elif [[ -n "$CONFIG_VAL" ]]; then
-                MAPPED_KEY="$(map_config_key "$CONFIG_KEY")"
-                if [[ -z "$MAPPED_KEY" ]]; then
-                    printf "[\e[33mWARN\e[0m] Unknown configuration key '%s'; ignoring.\n" "$CONFIG_KEY"
-                    continue
-                fi
-
-                if [[ -n "$FQBN_OPTIONS" ]]; then
-                    FQBN_OPTIONS="${FQBN_OPTIONS},${MAPPED_KEY}=${CONFIG_VAL}"
-                else
-                    FQBN_OPTIONS="${MAPPED_KEY}=${CONFIG_VAL}"
-                fi
+            elif [[ "$CONFIG_KEY" == "ADDITIONAL_OPTIONS" ]]; then
+                ADDITIONAL_OPTIONS="$CONFIG_VAL"
             fi
-
         fi
     done < "$LINUX_BUILD_CONF"
-
     if [[ -z "$PARSED_FQBN" ]]; then
         printf "[\e[31mFAIL\e[0m] FQBN is missing or empty in your config file.\n"
-        printf "Provided FQBN: '%s'\n\n" "$PARSED_FQBN"
-        printf "The FQBN must exactly match an FQBN returned by:\n"
-        printf "%s --config-file %s board listall\n\n" "$BINPATH" "$CONFIG_FILE"
         exit 1
     fi
-
     if ! fqbn_exists; then
         select_closest_match
     fi
+}
 
-    if [[ -n "$FQBN_OPTIONS" ]]; then
-        FINAL_FQBN="${PARSED_FQBN}:${FQBN_OPTIONS}"
+function validate_options() {
+    BOARD_DETAILS="$("$BINPATH" --config-file "$CONFIG_FILE" board details -b "$PARSED_FQBN")"
+    if [[ $? -ne 0 ]]; then
+        printf "[\e[31mFAIL\e[0m] Failed to get board details for %s\n" "$PARSED_FQBN"
+        exit 1
+    fi
+    UNKNOWN_OPTS=""
+    if [[ -n "$ADDITIONAL_OPTIONS" ]]; then
+        IFS_BAK="$IFS"
+        IFS=","
+        for OPT in $ADDITIONAL_OPTIONS; do
+            if ! printf '%s' "$BOARD_DETAILS" | grep -qF "$OPT"; then
+                if [[ -z "$UNKNOWN_OPTS" ]]; then
+                    UNKNOWN_OPTS="$OPT"
+                else
+                    UNKNOWN_OPTS="$UNKNOWN_OPTS, $OPT"
+                fi
+            fi
+        done
+        IFS="$IFS_BAK"
+    fi
+    if [[ -n "$UNKNOWN_OPTS" ]]; then
+        printf "the following option is unknown check config.txt\n%s\n" "$UNKNOWN_OPTS"
+        exit 1
+    fi
+    if [[ -n "$ADDITIONAL_OPTIONS" ]]; then
+        FINAL_FQBN="${PARSED_FQBN}:${ADDITIONAL_OPTIONS}"
     else
         FINAL_FQBN="${PARSED_FQBN}"
     fi
-}
-
-function mover() {
-    find "$ROOT_PATH" -maxdepth 1 -type f -name "$(basename "${SRC_CODE%.ino}").*" ! -name "$(basename "$SRC_CODE")" -exec mv -v {} "$ROOT_PATH/firmware/" \;
 }
 
 function prepare_firmware_dir() {
@@ -303,24 +221,19 @@ function prepare_firmware_dir() {
 function build_firmware() {
     printf "Starting build process...\n"
     printf "Final Configured FQBN: %s\n" "$FINAL_FQBN"
-
     mkdir -pv "$ROOT_PATH"
     mkdir -pv "$TMP_DIR/build"
     mkdir -pv "$TMP_DIR/cache"
-
     "$BINPATH" --config-file "$CONFIG_FILE" --verbose compile --fqbn "$FINAL_FQBN" --build-path "$TMP_DIR/build" --build-cache-path "$TMP_DIR/cache" --output-dir "$ROOT_PATH" "$SRC_CODE"
-
     COMPILE_STATUS=$?
-
     if [[ $COMPILE_STATUS -eq 0 ]]; then
-        mover
+        find "$ROOT_PATH" -maxdepth 1 -type f -name "$(basename "${SRC_CODE%.ino}").*" ! -name "$(basename "$SRC_CODE")" -exec mv -v {} "$ROOT_PATH/firmware/" \;
         printf "[\e[32m OK \e[0m] Compilation successful. Compiled binaries extracted to %s\n" "$ROOT_PATH/firmware"
     else
         printf "[\e[31mFAIL\e[0m] Compilation failed.\n"
     fi
-
     rm -vrf "$TMP_DIR"
-
+    rm -vrf "$ROOT_PATH/build"
     if [[ $COMPILE_STATUS -ne 0 ]]; then
         exit "$COMPILE_STATUS"
     fi
@@ -337,7 +250,7 @@ while [[ $# -gt 0 ]]; do
             BINPATH="$2"
             shift 2
         ;;
-
+        
         --toolchain-root)
             TOOLCHAIN_ROOT="$2"
             shift 2
@@ -366,6 +279,7 @@ while [[ $# -gt 0 ]]; do
             TMP_DIR="$ROOT_PATH/TMP"
             load_board_list
             parse_config
+            validate_options
             prep
             exit 0
         ;;
