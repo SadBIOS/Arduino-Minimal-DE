2c65ddbd33d1b96a93520c7d50c6283786320f87
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-09-09 13:55:55 +0600

    Update db_create.sh

--- runtime/db_create.sh
+++ runtime/db_create.sh
@@ -30,3 +30,16 @@ DB_SUB_OPTION=""
 function fqbn_symstrip() {
     printf '%s\n' "${1//[^[:alnum:]]/}"
 }
+function initialize_db() {
+    if [[ ! -d "$HWDB" ]]; then
+        mkdir -pv "$HWDB"
+    else
+        DB_DIRS=$(find "$HWDB" -mindepth 1 -maxdepth 1 -type d | wc -l)
+        DB_ENTRIES=$(find "$HWDB" -type f -name "*.txt" -exec wc -l {} + | grep total | awk '{print $1}')
+        DB_ENTRIES=${DB_ENTRIES:-0}
+        printf "Hardware Database Summary:\n"
+        printf "Total FQBNs registered: %s\n" "$DB_DIRS"
+        printf "Total entries documented: %s\n\n" "$DB_ENTRIES"
+    fi
+
+}
