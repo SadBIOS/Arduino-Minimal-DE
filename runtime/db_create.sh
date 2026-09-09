3da1fb75704a846edf0d0ddc9145443449e0455d
Md. Kowshik <168761417+SadBIOS@users.noreply.github.com>
2026-09-09 13:57:28 +0600

    Update db_create.sh

--- runtime/db_create.sh
+++ runtime/db_create.sh
@@ -200,3 +200,30 @@ function process_device() {
     fi
 
 }
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --binpath) 
+            BINPATH="$2" 
+            shift 2 
+        ;;
+
+        --toolchain-root) 
+            TOOLCHAIN_ROOT="$2" 
+            shift 2 
+        ;;
+
+        --config-file) 
+            CONFIG_FILE="$2" 
+            shift 2 
+        ;;
+
+        --approved-hwid-list) 
+            APPROVED_HWID_LIST="$2" 
+            shift 2 
+            initialize_db
+            prompt_fqbn
+            monitor_usb
+            process_device
+        ;;
+    esac
+done
